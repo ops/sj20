b05559093e814fc6f5bd8a80386260a2032b5e77
Olli Savia <ops@iki.fi>
2019-04-11 19:17:36 +0300

    Added main.s

---
+++ main.s
@@ -0,0 +1,24 @@
+;;;
+;;; sj20
+;;;
+
+        .include "cbm_kernal.inc"
+        .include "vic20.inc"
+
+        .export __LOADADDR__: absolute = 1
+        .segment "LOADADDR"
+        .addr   *+2
+
+        .segment "CODE"
+
+        .import sj20_init
+
+PTRSTR          := $CB1E        ; Print zero terminated sting
+
+        jsr     sj20_init
+        lda     #<banner
+        ldy     #>banner
+        jsr     PTRSTR
+        rts
+
+banner: .byte "sj20 activated", 13, 0
